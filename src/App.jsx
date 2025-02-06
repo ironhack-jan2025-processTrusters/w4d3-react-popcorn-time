db28c571930abba8dc4f86c653adbff6ef12cfcd
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-02-06 11:26:37 +0100

    refactor: display number of movies in Header (lift state up)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MovieList from "./components/MovieList"
 
+import movies from "./data/movies.json"
+
 function App() {
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+  const deleteMovie = (movieId) => {
+    const newListOfMovies = moviesToDisplay.filter((movie) => {
+      return movie.id !== movieId;
+    });
+    setMoviesToDisplay(newListOfMovies);
+  }
 
   return (
     <>
-      <Header />
-      <MovieList />
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <MovieList moviesArr={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </>
   )
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,32 +1,17 @@
-import { useState } from "react";
 import Movie from "./Movie";
-import movies from "../data/movies.json"
 import "./MovieList.css"
 
 
-function MovieList() {
-
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
-
-    
-    const deleteMovie = (movieId) => {
-        const newListOfMovies = moviesToDisplay.filter((movie) => {
-            return movie.id !== movieId;
-        });
-        setMoviesToDisplay(newListOfMovies);
-    }
-
-
+function MovieList(props) {
     return (
         <section className="MovieList">
-            <h2>Number of movies: {moviesToDisplay.length}</h2>
 
-            {moviesToDisplay.map((movieObj) => {
+            {props.moviesArr.map((movieObj) => {
                 return (
                     <Movie
                         key={movieObj.id}
                         movieDetails={movieObj}
-                        callbackToDelete={deleteMovie}
+                        callbackToDelete={props.callbackToDelete}
                     />)
             })}
         </section>
